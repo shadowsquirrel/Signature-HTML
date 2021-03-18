0654194297e5eed72fdfa01d3cc3a2f6f3e0d626
shadowsquirrel <cnelebi@gmail.com>
2021-03-18 16:31:50 +0100

    dynamic pen size (much uglier than I hoped for)

--- js/stest.js
+++ js/stest.js
@@ -7,26 +7,37 @@ setTimeout(()=>{
 
 var sketchpad = new Sketchpad({
   element: '#sketchpad',
-  width: 400,
+  width: 800,
   height: 400,
 });
 
 var myPenSize;
 
+var dynamicPenSize = true;
+
 var v2S = function(v) {
 
-    var L, k , x0, size;
+    if(dynamicPenSize) {
 
-    // parameter values
-    L = 20;
-    k = 0.01;
-    x0 = -295;
+        var L, k, i, x0, size;
 
-    size = L / (1 + Math.exp(-k * (-v-x0)));
+        // parameter values
+        L = 15;
+        k = 0.005;
+        i = 5;
+        x0 = -800;
 
+        size = (L / (1 + Math.exp(-k * (-v-x0))) ) + i;
 
+        size = Math.ceil(size);
 
-    return size;
+        return size;
+
+    } else {
+
+        return 10;
+
+    }
 
 }
 
@@ -34,9 +45,11 @@ var v2S = function(v) {
 
 var update = function(velocity) {
     var v = velocity.velocity();
+    var s = v2S(v);
     // console.log(v2S(v));
     // console.log(typeof(v));
-    $('#myPenSize').html('size: ' + v2S(v));
+    $('#myPenSize').html('size: ' + s);
+    sketchpad.penSize = s;
     $('#velocity').html('linear speed: ' + v);
     $('#velocityX').html('x: ' + velocity.velocityX());
     $('#velocityY').html('y: ' + velocity.velocityY());
